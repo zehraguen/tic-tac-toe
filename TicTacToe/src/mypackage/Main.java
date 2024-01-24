fd7beb9bff410f2a9858bcb24c53505c64319f14
Zehra Gün <90135499+zehraguen@users.noreply.github.com>
2024-01-24 21:27:49 +0300

    classes
    - Main.java created
    - TicTacToe.java created

---
+++ TicTacToe/src/mypackage/Main.java
@@ -0,0 +1,17 @@
+package mypackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		printGrid();
+	}
+
+	public static void printGrid(){
+		System.out.println();
+		System.out.println("  x | o |  ");
+		System.out.println("  - + - + -");
+		System.out.println("    | x |  ");
+		System.out.println("  - + - + -");
+		System.out.println("    |   | x");
+	}
+}
---
+++ TicTacToe/src/mypackage/TicTacToe.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public class TicTacToe {
+	private char[][] grid;
+	
+	public TicTacToe(){
+		grid = new char[3][3];
+	}
+}
