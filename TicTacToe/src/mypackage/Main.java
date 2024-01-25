1987e387283e74102bcff45cc15f8eacf3b87619
Zehra Gün <90135499+zehraguen@users.noreply.github.com>
2024-01-25 14:27:39 +0300

    put a character
    - can put a character into the grid

--- TicTacToe/src/mypackage/Main.java
+++ TicTacToe/src/mypackage/Main.java
@@ -1,17 +1,39 @@
 package mypackage;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		printGrid();
+		TicTacToe newGame = new TicTacToe();
+		printGrid(newGame);
+		makeMove(newGame);
+		printGrid(newGame);
 	}
 
-	public static void printGrid(){
+	public static void printGrid(TicTacToe game){
+//		System.out.println();
+//		System.out.println("  x | o |  ");
+//		System.out.println("  - + - + -");
+//		System.out.println("    | x |  ");
+//		System.out.println("  - + - + -");
+//		System.out.println("    |   | x");
+		
 		System.out.println();
-		System.out.println("  x | o |  ");
+		System.out.println();
+		System.out.println("  "+ game.getGrid(0, 0) +" | "+ game.getGrid(1, 0) +" | "+ game.getGrid(2, 0) );
 		System.out.println("  - + - + -");
-		System.out.println("    | x |  ");
+		System.out.println("  "+ game.getGrid(0, 1) +" | "+ game.getGrid(1, 1) +" | "+ game.getGrid(2, 1) );
 		System.out.println("  - + - + -");
-		System.out.println("    |   | x");
+		System.out.println("  "+ game.getGrid(0, 2) +" | "+ game.getGrid(1, 2) +" | "+ game.getGrid(2, 2) );
+		System.out.println();
+		System.out.println();
+	}
+	
+	public static void makeMove(TicTacToe game) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("make your move: ");
+		int place = sc.nextInt();
+		game.setGrid('x', place);
 	}
 }
--- TicTacToe/src/mypackage/TicTacToe.java
+++ TicTacToe/src/mypackage/TicTacToe.java
@@ -5,5 +5,49 @@ public class TicTacToe {
 	
 	public TicTacToe(){
 		grid = new char[3][3];
+		
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				grid[i][j]= ' ';
+			}
+		}
+	}
+	
+	public char getGrid(int i, int j){ 
+		return grid[i][j];
+	}
+	
+	public void setGrid(char n, int p) {
+		switch(p) {
+			case(1):
+				grid[0][0] = n;
+				break;
+			case(2):
+				grid[1][0] = n;
+				break;
+			case(3):
+				grid[2][0] = n;
+				break;
+			case(4):
+				grid[0][1] = n;
+				break;
+			case(5):
+				grid[1][1] = n;
+				break;
+			case(6):
+				grid[2][1] = n;
+				break;
+			case(7):
+				grid[0][2] = n;
+				break;
+			case(8):
+				grid[1][2] = n;
+				break;
+			case(9):
+				grid[2][2] = n;
+				break;
+			default:
+				
+		}
 	}
 }
